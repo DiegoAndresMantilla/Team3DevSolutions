5d1d5c5e0f8266da257036c8299ffd2003753f59
Esteban Mateo Loachamin Aldaz <107655020+EstebanLoa@users.noreply.github.com>
2023-01-18 22:37:06 -0500

    Create Routes for iSoftware React

--- 06-Code/03WebClient/FrontEnd V1.0 iSoftware/src/routes/Routes.js
+++ 06-Code/03WebClient/FrontEnd V1.0 iSoftware/src/routes/Routes.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Principal from '../pages/principal';
-import ListInvoices from '../pages/invoices'
-import MenuInvoices from '../pages/invoiceMenu'
-import ListOrders from '../pages/orders'
-import MenuOrders from '../pages/orderMenu'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Principal from "../pages/principal";
+import ListInvoices from "../pages/invoices";
+import MenuInvoices from "../pages/invoiceMenu";
+import ListUsers from "../pages/users";
+import MenuUsers from "../pages/userMenu";
+import ListProducts from "../pages/products";
+import MenuProducts from "../pages/productMenu";
+import ListOrders from "../pages/orders";
+import MenuOrders from "../pages/orderMenu";
+import FormProducts from "../pages/productForm";
+import FormUsers from "../pages/userForm";
+import FormInvoices from "../pages/invoiceForm";
+import FormOrders from "../pages/orderForm";
 
 function App() {
-  
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<Principal/>} />
-        <Route exact path='/orders' element={<ListOrders/>} />
-        <Route exact path='/ordersMenu' element={<MenuOrders/>} />
-        <Route exact path='/invoices' element={<ListInvoices/>} />
-        <Route exact path='/invoicesMenu' element={<MenuInvoices/>} />
-    </Routes>
+        <Route exact path="/" element={<Principal />} />
+        <Route exact path="/invoices" element={<ListInvoices />} />
+        <Route exact path="/invoicesMenu" element={<MenuInvoices />} />
+        <Route exact path="/users" element={<ListUsers />} />
+        <Route exact path="/usersMenu" element={<MenuUsers />} />
+        <Route exact path="/products" element={<ListProducts />} />
+        <Route exact path="/productsMenu" element={<MenuProducts />} />
+        <Route exact path="/orders" element={<ListOrders />} />
+        <Route exact path="/ordersMenu" element={<MenuOrders />} />
+        <Route exact path="/productsForm" element={<FormProducts />} />
+        <Route exact path="/usersForm" element={<FormUsers />} />
+        <Route exact path="/invoicesForm" element={<FormInvoices />} />
+        <Route exact path="/ordersForm" element={<FormOrders />} />
+      </Routes>
     </Router>
   );
 }
