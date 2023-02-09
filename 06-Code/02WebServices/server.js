6a3c1f07cf7a92502cfcbb6568a954dc27762b63
Ligia Maricela Maldonado Paredes <85421504+lmmaldonado1@users.noreply.github.com>
2023-02-08 23:53:33 -0500

    Create a nre routes from api isfotware

--- 06-Code/02WebServices/server.js
+++ 06-Code/02WebServices/server.js
@@ -3,8 +3,10 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const portParemeter = 3003;
 const EndPoints = require("./api/EndPoints");
+const cors = require("cors");
 
 var app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/iSoftware", EndPoints);
@@ -18,6 +20,6 @@ mongoose.connect(
     err && console.log("Error al conectarse a la base de datos");
     app.listen(portParemeter, () => {
       console.log(`Server is running on port ${portParemeter}`);
-    });
-  }
+    });
+  }
 );
